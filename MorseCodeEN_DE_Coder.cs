83c1ed8d048a9974403ae70be58c91a176e38984
ShimGak <sg853@canterbury.ac.uk>
2022-03-13 15:39:20 +0000

    added dictionary
    added all letters and numbers

---
+++ MorseCodeEN_DE_Coder.cs
@@ -0,0 +1,48 @@
+namespace MorseCodeEN_DE_Coder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary < string, string>();
+            dictionary.Add("1", ".----");
+            dictionary.Add("2", "..---");
+            dictionary.Add("3", "...--");
+            dictionary.Add("4", "....-");
+            dictionary.Add("5", ".....");
+            dictionary.Add("6", "-....");
+            dictionary.Add("7", "--...");
+            dictionary.Add("8", "---..");
+            dictionary.Add("9", "----.");
+            dictionary.Add("0", "-----");
+            dictionary.Add(" ", " ");
+            dictionary.Add("a", ".-");
+            dictionary.Add("b", "-...");
+            dictionary.Add("c", "-.-.");
+            dictionary.Add("d", "-..");
+            dictionary.Add("e", ".");
+            dictionary.Add("f", "..-.");
+            dictionary.Add("g", "--.");
+            dictionary.Add("h", "....");
+            dictionary.Add("i", "..");
+            dictionary.Add("j", ".---");
+            dictionary.Add("k", "-.-");
+            dictionary.Add("l", ".-..");
+            dictionary.Add("m", "--");
+            dictionary.Add("n", "-.");
+            dictionary.Add("o", "---");
+            dictionary.Add("p", ".--.");
+            dictionary.Add("q", "--.-");
+            dictionary.Add("r", ".-.");
+            dictionary.Add("s", "...");
+            dictionary.Add("t", "-");
+            dictionary.Add("u", "..-");
+            dictionary.Add("v", "...-");
+            dictionary.Add("w", ".--");
+            dictionary.Add("x", "-..-");
+            dictionary.Add("y", "-.--");
+            dictionary.Add("z", "--..");
+
+        }
+    }
+}
